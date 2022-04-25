3df52977659597ea494d35728127394fa17189b6
MigoVR <hernandezmiguel0521@gmail.com>
2022-04-24 20:14:40 -0400

    read/write example finished for reference

--- Examples/ReadWriteExample/ReadWrite/ReadWrite.ino
+++ Examples/ReadWriteExample/ReadWrite/ReadWrite.ino
@@ -17,13 +17,21 @@
    To read data from the flash memory:
    EEPROM.read(address);
 
+   ___________________________________________________________________
+
+   This code reads and writes from the first address of the flash memory located 
+   on the ESP32 board. It increments a value as fast as rw_delay lets it. 
+   To change the rate of read/write, change rw_delay (in seconds). 
 */
+
+
 
 #define EEPROM_SIZE 1 //Change 1 to 5 for number of data things we need. 
 
 // Variables
 int data = 0;
-int counter = 0; 
+int counter = 0;
+const int rw_delay = 60; // number of seconds per write/read 
 
 
 void setup() {
@@ -46,7 +54,7 @@ void loop() {
 
   EEPROM.write(0, data); 
   EEPROM.commit(); 
-  delay(1*60*1000); // Delay write every minute.   
+  delay(1000*rw_delay);   
   
 
 }
