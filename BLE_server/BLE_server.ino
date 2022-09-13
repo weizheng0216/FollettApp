65d144a6983df461603a379c4c5cacc94094e161
MigoVR <hernandezmiguel0521@gmail.com>
2022-09-13 15:51:32 -0400

    create advertisement objects for other data points

--- BLE_server/BLE_server.ino
+++ BLE_server/BLE_server.ino
@@ -11,58 +11,178 @@
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+/**
+ * @brief Updates a given array to contain the values extracted from the ice machine
+ *
+ * @param myArray size 8 array that will hold values for mode, amp rate, error states, etc.
+ */
+void getData(byte data[])
+{
+
+  byte request[] = {
+      90, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 152, 171};
+  byte reply[170]; // was 167
+  byte i;
+  bool flag = false;
+
+  Serial.write(request, 105); // was 102
+
+  unsigned long currentMillis = millis();
+  while (!Serial.available())
+  {
+    if (millis() - currentMillis >= 500)
+    {
+      flag = true;
+      break;
+    }
+    delay(0);
+  }
+
+  Serial.readBytes(reply, 170); // was 167
+
+  if (!flag)
+  {
+    // condense data
+    // data[0] = reply[133]; //amps lowbyte was 130
+    // data[1] = reply[134]; //amps highbyte was 131
+    data[0] = reply[133]; // amps lowbyte was 130
+    data[1] = reply[134]; // amps highbyte was 131
+    for (i = 0; i < 8; i++)
+    {
+      bitWrite(data[2], i, reply[135 + i]);
+    } // merge din0-7 into 1 byte was 132
+    data[3] = 0;
+    for (i = 0; i < 1; i++)
+    {
+      bitWrite(data[3], i, reply[143 + i]);
+    }                     // merge din8-12 into 1 byte was 140 --- only 1st byte still valid, following 4 bytes are new auger current min/max
+    data[4] = reply[148]; // dipswitches was 145
+    data[5] = reply[149]; // dout0 was 146
+    // ignore dout8,16,24
+    data[6] = reply[153]; // errors lowbyte was 150
+    data[7] = reply[154]; // errors highbyte was 151
+    data[8] = reply[155]; // mymode was 152
+  }
+  else
+  {
+    data[0] = 255;
+    data[1] = 255;
+  }
+}
+
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define CHARACTERISTIC_UUID_2 "beb5483e-36e1-4688-b7f5-ea07361b26a9"
-BLECharacteristic *pCharacteristic;
-BLECharacteristic *pCharacteristic2;
-uint8_t to_send[7][2] = {{9,79},{10,99},{11,67},{12,88},{13,120},{14,131}, {15, 111}};
-uint8_t temp[12] = {1,1,0,0,1,0,1,0,0,0,0,1};
+// BLECharacteristic *pCharacteristic;
+// BLECharacteristic *pCharacteristic2; 
+BLECharacteristic *ampsLow;
+BLECharacteristic *ampsHigh;
+BLECharacteristic *mergedin0_7;
+BLECharacteristic *mergedin8_12;
+BLECharacteristic *dipSwitches;
+BLECharacteristic *dout0;
+BLECharacteristic *errLow;
+BLECharacteristic *errHigh;
+BLECharacteristic *mode;
 
-void setup() {
+uint8_t to_send[7][2] = {{9, 79}, {10, 99}, {11, 67}, {12, 88}, {13, 120}, {14, 131}, {15, 111}};
+uint8_t temp[12] = {1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1};
+
+
+void setup()
+{
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
 
   BLEDevice::init("Follett Ice Machine");
   BLEServer *pServer = BLEDevice::createServer();
   BLEService *pService = pServer->createService(SERVICE_UUID);
-  pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
-                                      
-  pCharacteristic2 = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID_2,
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
+
+  // Intialize Bluetooth Characteristics
+  // pCharacteristic = pService->createCharacteristic(
+  //     CHARACTERISTIC_UUID,
+  //     BLECharacteristic::PROPERTY_READ |
+  //         BLECharacteristic::PROPERTY_WRITE);
+
+  // pCharacteristic2 = pService->createCharacteristic(
+  //     CHARACTERISTIC_UUID_2,
+  //     BLECharacteristic::PROPERTY_READ |
+  //         BLECharacteristic::PROPERTY_WRITE);
+
+  ampsLow = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+  ampsHigh = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+  mergedin0_7 = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+  mergedin8_12 = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+  dipSwitches = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+  dout0 = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+  errLow = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+  errHigh = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+  mode = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+
+  // Start Bluetooth Service
   pService->start();
   // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
   Serial.println("Characteristic defined! Now you can read it in your phone!");
 
-//  uint8_t to_send[6][2] = {{2,102},{4,100},{5,88},{6,123},{7,99},{8,111}};
-//  pCharacteristic->setValue((uint8_t *)to_send, sizeof(to_send)/sizeof(to_send[0]) * 2);
-//  pCharacteristic->notify();
-  
+  //  uint8_t to_send[6][2] = {{2,102},{4,100},{5,88},{6,123},{7,99},{8,111}};
+  //  pCharacteristic->setValue((uint8_t *)to_send, sizeof(to_send)/sizeof(to_send[0]) * 2);
+  //  pCharacteristic->notify();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  
+
   to_send[5][0] = to_send[5][0] + 1;
   to_send[5][1] = to_send[5][1] + 10;
-  
-  temp[random(11)] =0;
-  pCharacteristic->setValue((uint8_t *)to_send, sizeof(to_send)/sizeof(to_send[0]) * 2);
-  pCharacteristic2->setValue((uint8_t *)temp, 12);
-  pCharacteristic->notify();
-  pCharacteristic2->notify();
+
+  // temp[random(11)] = 0;
+  // pCharacteristic->setValue((uint8_t *)to_send, sizeof(to_send) / sizeof(to_send[0]) * 2);
+  // pCharacteristic2->setValue((uint8_t *)temp, 12);
+  // pCharacteristic->notify();
+  // pCharacteristic2->notify();
+
+  byte data[9];  
+  getData(data);
+
+
+  ampsLow->setValue((uint8_t *)to_send, sizeof(to_send) / sizeof(to_send[0]) * 2);
+
+
+
+
   delay(5000);
 }
